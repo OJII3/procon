d021ed12a2ba4735de5289a5fcf1b9b7404b0b43
OJII3 <kawaisounayuriko@gmail.com>
2022-11-06 21:47:00 +0900

    ac

--- abc276/b/main.py
+++ abc276/b/main.py
@@ -1,29 +1,15 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().rstrip().split())
-a, b = list(range(0, m)), list(range(0, m))
-for i in range(1, m):
-    a[i-1], b[i-1] = map(int, sys.stdin.readline().rstrip().split())
+cities = [[] for _  in range(0, n)]
 
-print(n, m, a, b)
+for i in range(0, m):
+  ai, bi = map(int, sys.stdin.readline().rstrip().split())
+  cities[ai-1].append(str(bi))
+  cities[bi-1].append(str(ai))
 
-cities = []
-for i in range(1, n+1):
-    l = []
-    num = 0
-    
-    #search i
-    for j in range(0, m):
-        try:
-            if(a.index(i, j) >= 0):
-                print(i, a.index(i, j), b[a.index(i, j)])
-                num += 1
-                l.append(b[a.index(i, j)])
-        except:
-            None
-
-    
-    l.insert(0, num)
-    cities.append(l)
-    
-print(cities)
+for i in range(0, n):
+  cities[i].sort(key=int)
+  cities[i].insert(0, str(cities[i].__len__()))
+  print(' '.join(cities[i]))
+  
