9c3e7b434f4f1bac8b2957878451e7ad54941b7b
OJII3 <kawaisounayuriko@gmail.com>
2022-11-10 16:24:50 +0900

    ac

--- abc276/c/main.py
+++ abc276/c/main.py
@@ -6,11 +6,10 @@ p = list(map(str, sys.stdin.readline().rstrip().split()))
 
 for i in range(n):
   last = len(p) -1
-  if p[last-i-1] > p[last-i]:
+  if int(p[last-i-1]) > int(p[last-i]):
     formar = p[:last-i-1]
     latter = p[last-i-1:]
-    
-    latter_sorted = sorted(latter, key=int, reverse=True)
+    latter_sorted = sorted(latter, key=int, reverse=False)
     next_large = latter_sorted[latter_sorted.index(latter[0])-1]
     latter.remove(next_large)
     print(' '.join(formar + [next_large] + sorted(latter, key=int, reverse=True)))
---
+++ abc276/d/main.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+a = list(map(int, sys.stdin.readline().strip().split()))
+
+m2 = list(range(n))
+m3 = list(range(n))
+
+
+    
+    
+print(a, base, m2, m3)
