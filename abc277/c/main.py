17eb8d3b814375df9daa5749212db994a13c594d
OJII3 <kawaisounayuriko@gmail.com>
2022-11-18 19:58:26 +0900

    Change Folder Trees

--- abc277/c/main.py
+++ abc277/c/main.py
@@ -7,7 +7,4 @@ b = list(range(n))
 for i in range(n):
   a[i], b[i] = map(int, sys.stdin.readline().rstrip().split())
   
-def search_next(candits):
-  for candit in candits:
-    last = 0
-    for i 
+print(a, b)
