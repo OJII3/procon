cdbb1f04f9eaff518c1df301cacf15266177ceee
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-07-08 08:23:06 +0900

    update

--- abc308/d/main.cpp
+++ abc308/d/main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main() {
   int h, w;
   cin >> h >> w;
-  vector<vector<string>> s(h, vector<string>(w));
+  vector<string> s(h);
+
+  for (int i = 0; i < h; ++i) {
+    cin >> s[i];
+    cout << s[i] << endl;
+  }
+  if (s[0][0] != 's') {
+    cout << "No" << endl;
+    return 0;
+  }
 
   vector<char> next(256);
   next['s'] = 'n';
@@ -13,6 +22,12 @@ int main() {
   next['u'] = 'k';
   next['k'] = 'e';
   next['e'] = 's';
+
+  vector<vector<bool>> seen(h, vector<bool>(w));
+
+  auto dfs = [&](auto &dfs, int y, int x) -> void {
+
+  };
 
   return 0;
 }
