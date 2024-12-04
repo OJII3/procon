2cf19655e7da2ca14de05eb39e40c00d02340569
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-04 14:00:42 +0900

    ac

--- src/abc/abc238/b/main.cpp
+++ src/abc/abc238/b/main.cpp
@@ -12,21 +12,23 @@ int main(void) {
         if (i == 0)
             cut[i] = tmp;
         else {
-            cut[i] = cut[i - 1] + tmp;
+            cut[i] = (cut[i - 1] + tmp) % 360;
         }
     }
 
-    for (int i = 0; i < n; ++i) {
-        cut[i] = cut[i] % 360;
-    }
     cut.push_back(360);
 
     sort(cut.begin(), cut.end());
 
     int max = 0;
-    for (int i = 0; i < n; ++i) {
-        int tmp = cut[i + 1] - cut[i];
-        max = max < tmp ? tmp : max;
+    for (int i = 0; i < n + 1; ++i) {
+        if (i == 0) {
+            int tmp = cut[i];
+            max = max < tmp ? tmp : max;
+        } else {
+            int tmp = cut[i] - cut[i - 1];
+            max = max < tmp ? tmp : max;
+        }
     }
 
     cout << max << endl;
